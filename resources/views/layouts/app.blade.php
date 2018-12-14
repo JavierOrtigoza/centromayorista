d41d59870940a48d9fabae31627566705a32d692
Javier Ortigoza <javier.otza@gmail.com>
2018-12-13 22:40:14 -0300

    se agregó información de la página

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,9 @@
 <html lang="es">
 <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
+    <meta name="description" content="En Centromayorista somos importadores de una gran línea de productos,
+    algunas de ellas son: Herrmientas TOTAL, Accesorios de pc y audio CROWN, Electrdomésticos GEMEI, además
+    de completas líneas de productos de tocador, cosmética y perfumería">
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="{{asset ('/imgs/centromayorista.ico')}}">
